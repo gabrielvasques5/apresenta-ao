9c5070a3830b37e4983c6c17135c1cece93222b5
Gabriel Xavier Vasques <gabriel.vasques2907@gmail.com>
2026-05-17 14:22:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Gabriel Xavier Portfolio Loaded');
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    header.classList.toggle("scroll", window.scrollY > 50);
+
+});
